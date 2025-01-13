09f5a7ecfbecce201e59250d81ecd41f11415f7b
SajithaS98 <sayafeb142k19@gmail.com>
2025-01-13 15:53:10 +0530

    added_admin_and_permissions

--- CollegeApp/permissions.py
+++ CollegeApp/permissions.py
@@ -1,5 +1,15 @@
 from rest_framework.permissions import BasePermission
 
+
+class IsAdmin(BasePermission):
+    """
+    Allows access only to users with the admin role.
+    """
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_admin
+
+
+
 class IsAdminOrHOD(BasePermission):
     """
     Custom permission to allow only admins and HODs.
